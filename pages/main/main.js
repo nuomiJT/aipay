23ff4c290051b7db534dc25462b0ac31ad141ba9
jt <947745775@qq.com>
2019-02-22 15:00:22 +0800

    mian

--- pages/main/main.js
+++ pages/main/main.js
@@ -5,16 +5,24 @@ const app = getApp()
 
 Page({
   data:{
+    radioCheckVal:0,
     class:[
       {}
     ]
   },
 
-  //图片点击事件处理函数测试
+  //图片点击事件处理函数
   bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
-  }
+  },
+
+ //年级单选点击事件处理函数
+  radioCheckedChange:function(e){
+    this.setData({
+      radioCheckVal:e.detail.value
+    })
+  },
 })
 
