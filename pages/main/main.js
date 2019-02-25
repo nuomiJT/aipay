1448bb41f2d0bce3c9c686311e8b9228a9d33324
jt <947745775@qq.com>
2019-02-25 18:07:10 +0800

    class

--- pages/main/main.js
+++ pages/main/main.js
@@ -12,7 +12,8 @@ Page({
   },
 
   //图片点击事件处理函数
-  bindViewTap: function () {
+  bindImageTo: function (e) {
+    console.log('bindImageTo', e)
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -20,9 +21,18 @@ Page({
 
  //年级单选点击事件处理函数
   radioCheckedChange:function(e){
+    console.log('radioCheckedChange',e)
     this.setData({
       radioCheckVal:e.detail.value
     })
   },
-})
 
+  //季节班级点击跳转事件处理函数
+  bindToClass:function(e){
+    console.log('bindToClass',e)
+    wx.navigateTo({
+      url: '../class/class',
+    })
+  }
+
+})
